1088ad7b9798226ca8e3ee85b1676c83299c660a
renanaik <renanaik17@gmail.com>
2025-04-14 15:57:39 +0530

    Add files via upload

---
+++ 3.5.cpp
@@ -0,0 +1,33 @@
+/* THIS PROGRAM IS PREPARED BY 24CE066_RENA
+   3.5 FINDING SUPERDIGIT */
+#include <iostream>
+using namespace std;
+long long superdigit(string n, int k)
+{
+    long long sum = 0;
+    for (char digit : n)
+    {
+        sum += digit - '0';
+    }
+    sum *= k;
+    while (sum >= 10)
+    {
+        long long x = 0;
+        while (sum > 0) {
+            x += sum % 10;
+            sum /= 10;
+        }
+        sum = x;
+    }
+    return sum;
+}
+int main() {
+    string x;
+    int k;
+    cout << "Enter the Number as a String: ";
+    cin >> x;
+    cout << "Enter the Repetation Factor : ";
+    cin >> k;
+    cout << "Super Digit: " << superdigit(x, k)<<endl;
+    cout<<"Rena Naik-24CE066";
+}
