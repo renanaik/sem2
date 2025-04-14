a17915634b56597ece3254fb5958560ac6141f21
renanaik <renanaik17@gmail.com>
2025-04-14 15:52:53 +0530

    Add files via upload

---
+++ 2.1.cpp
@@ -0,0 +1,90 @@
+/*THIS PROGRAM IS PREPARED BY 24CE066_REN
+2.1 Reactangle Calculation*/
+#include <iostream>
+using namespace std;
+class rectangle
+{
+    int length,width;
+public:
+    void dimensions(int l,int w)
+    {
+        length=l;
+        width=w;
+    }
+    int area()
+    {
+       int area;
+       area=length*width;
+       return area;
+    }
+    int perimeter()
+    {
+        int peri;
+        peri=2*(length+width);
+        return peri;
+    }
+     void displaydetails()
+    {
+        cout << " Area: " << area() << "  Perimeter: " << perimeter() << endl;
+    }
+};
+int main()
+{
+    rectangle r[50];
+    int count = 0;
+    char choice;
+    do
+    {
+        double length, width;
+        cout << "Enter length of rectangle: ";
+        cin >> length;
+        cout << "Enter width of rectangle: ";
+        cin >> width;
+        r[count].dimensions(length, width);
+        count++;
+
+        cout << "Do you want to add another rectangle? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' && count < 50);
+    cout << "\nRectangle Details:" << endl;
+    for (int i = 0; i < count; i++)
+    {
+        cout << "Rectangle " << i + 1 << "- ";
+        r[i].displaydetails();
+    }
+//To Update Any Rectangle's Dimension:
+    char updatechoice;
+    do
+    {
+        int index;
+        cout << "Do you want to update any rectangle's dimensions? (y/n): ";
+        cin >> updatechoice;
+        if (updatechoice == 'y')
+        {
+            cout << "Enter rectangle number to update (1 to " << count << "): ";
+            cin >> index;
+            if (index > 0 && index <= count)
+            {
+                double newLength, newWidth;
+                cout << "Enter new length: ";
+                cin >> newLength;
+                cout << "Enter new width: ";
+                cin >> newWidth;
+                r[index - 1].dimensions(newLength, newWidth);
+                cout << "Updated details:" << endl;
+                r[index - 1].displaydetails();
+            }
+            else
+            {
+                cout << "Invalid rectangle number!" << endl;
+            }
+        }
+    } while (updatechoice == 'y');
+
+    cout<<endl<<"THANK YOU!!"<<endl;
+    cout<<"Rena Naik-24CE066";
+
+    return 0;
+}
+
+
