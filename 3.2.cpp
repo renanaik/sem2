a945d12d3e3fbb451c860bd38712526445b95c26
renanaik <renanaik17@gmail.com>
2025-04-14 15:56:13 +0530

    Add files via upload

---
+++ 3.2.cpp
@@ -0,0 +1,40 @@
+/* THIS PROGRAM IS PREPARED BY 24CE066_RENA
+   3.2 RECURSION AND ITTRATION COMPARISION */
+#include <iostream>
+using namespace std;
+int iterrative(int a[],int n)
+{
+    int sum;
+    for (int i=0; i<n; i++)
+    {
+        sum=sum+a[i];
+    }
+    return sum;
+}
+int reccursive(int a[],int n)
+{
+    if (n==0)
+    {
+        return 0;
+    }
+    return a[n-1] + reccursive(a,n-1);
+}
+int main()
+{
+    int n;
+    cout<<"enter the number of elements:";
+    cin>>n;
+    int* x = new int[n];
+    cout<<"enter "<<n<<" elements:"<<endl;
+    for (int i=0; i<n; i++)
+    {
+        cin>>x[i];
+    }
+    int i=iterrative(x,n);
+    int r=reccursive(x,n);
+    cout<<"Sum using Ittration: "<<i<<endl;
+    cout<<"Sum using Reccursion: "<<r<<endl;
+    delete[] x;
+    cout<<endl<<"Rena Naik-24CE066";
+
+}
